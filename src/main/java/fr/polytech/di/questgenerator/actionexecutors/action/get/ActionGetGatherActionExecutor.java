3a99a313e3112db80674bc7bc34fbc1dafbd6666
Thomas Couchoud <zerderr@gmail.com>
2016-02-01 21:54:11 +0100

    Add Get logic

--- src/main/java/fr/polytech/di/questgenerator/actionexecutors/action/get/ActionGetGatherActionExecutor.java
+++ src/main/java/fr/polytech/di/questgenerator/actionexecutors/action/get/ActionGetGatherActionExecutor.java
@@ -4,7 +4,6 @@ import fr.polytech.di.questgenerator.enums.ActionType;
 import fr.polytech.di.questgenerator.enums.ObjectiveType;
 import fr.polytech.di.questgenerator.interfaces.ActionExecutor;
 import fr.polytech.di.questgenerator.objects.Action;
-import fr.polytech.di.questgenerator.objects.DataHandler;
 import fr.polytech.di.questgenerator.objects.ObjectiveHelper;
 import fr.polytech.di.questgenerator.objects.Quest;
 import fr.polytech.di.questgenerator.objects.xml.XMLStringObjectiveElement;
@@ -20,9 +19,15 @@ public class ActionGetGatherActionExecutor implements ActionExecutor
 	@Override
 	public Quest generateQuest(int depth, Optional<HashMap<ObjectiveType, XMLStringObjectiveElement>> objectives)
 	{
-		Action actionGoto = new Action(this.getClass(), depth, ActionType.GOTO, buildObjective(objectives, new ObjectiveHelper(OBJECTIVE, LOC_OBJECTIVE, DataHandler.getRandomArea())));
-		Action actionGather = new Action(this.getClass(), depth, ActionType.GATHER, buildObjective(objectives, new ObjectiveHelper(OBJECTIVE, OBJ_GET, DataHandler.getRandomObject())), false);
+		Action actionGoto = new Action(this.getClass(), depth, ActionType.GOTO, buildObjective(objectives, new ObjectiveHelper(OBJECTIVE, LOC_OBJECTIVE)));
+		Action actionGather = new Action(this.getClass(), depth, ActionType.GATHER, buildObjective(objectives, new ObjectiveHelper(OBJECTIVE, OBJ_GET)), false);
 
 		return new Quest(actionGoto, actionGather);
+	}
+
+	@Override
+	public boolean isActionAllowed(Optional<HashMap<ObjectiveType, XMLStringObjectiveElement>> objectives)
+	{
+		return !objectives.isPresent() || (!objectives.get().containsKey(OBJ_GET) || !objectives.get().containsKey(LOC_OBJECTIVE)) && objectives.get().get(OBJ_GET).isInPath("object/gatherable/*") && objectives.get().get(LOC_OBJECTIVE).isInPath("area/*");
 	}
 }
