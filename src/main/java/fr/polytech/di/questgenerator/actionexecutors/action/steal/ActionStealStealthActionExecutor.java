b6bf9302ab0b871a74ff4227d08a97bf4185b269
Thomas Couchoud <zerderr@gmail.com>
2016-02-01 22:08:55 +0100

    Add steal logic

--- src/main/java/fr/polytech/di/questgenerator/actionexecutors/action/steal/ActionStealStealthActionExecutor.java
+++ src/main/java/fr/polytech/di/questgenerator/actionexecutors/action/steal/ActionStealStealthActionExecutor.java
@@ -4,15 +4,12 @@ import fr.polytech.di.questgenerator.enums.ActionType;
 import fr.polytech.di.questgenerator.enums.ObjectiveType;
 import fr.polytech.di.questgenerator.interfaces.ActionExecutor;
 import fr.polytech.di.questgenerator.objects.Action;
-import fr.polytech.di.questgenerator.objects.DataHandler;
 import fr.polytech.di.questgenerator.objects.ObjectiveHelper;
 import fr.polytech.di.questgenerator.objects.Quest;
 import fr.polytech.di.questgenerator.objects.xml.XMLStringObjectiveElement;
 import java.util.HashMap;
 import java.util.Optional;
-import static fr.polytech.di.questgenerator.enums.ObjectiveType.OBJECTIVE;
-import static fr.polytech.di.questgenerator.enums.ObjectiveType.OBJ_GET;
-import static fr.polytech.di.questgenerator.enums.ObjectiveType.PNJ;
+import static fr.polytech.di.questgenerator.enums.ObjectiveType.*;
 
 /**
  * Created by COUCHOUD Thomas & COLEAU Victor.
@@ -22,11 +19,9 @@ public class ActionStealStealthActionExecutor implements ActionExecutor
 	@Override
 	public Quest generateQuest(int depth, Optional<HashMap<ObjectiveType, XMLStringObjectiveElement>> objectives)
 	{
-		XMLStringObjectiveElement objectivePNJ = DataHandler.getRandomPNJ();
-
-		Action actionGoto = new Action(this.getClass(), depth, ActionType.GOTO, buildObjective(objectives, new ObjectiveHelper(OBJECTIVE, PNJ, objectivePNJ)));
-		Action actionStealth = new Action(this.getClass(), depth, ActionType.STEALTH, buildObjective(objectives, new ObjectiveHelper(OBJECTIVE, PNJ, objectivePNJ)), false);
-		Action actionTake = new Action(this.getClass(), depth, ActionType.TAKE, buildObjective(objectives, new ObjectiveHelper(PNJ, PNJ, objectivePNJ), new ObjectiveHelper(OBJ_GET, OBJ_GET, DataHandler.getRandomObject())), false);
+		Action actionGoto = new Action(this.getClass(), depth, ActionType.GOTO, buildObjective(objectives, new ObjectiveHelper(OBJECTIVE, PNJ)));
+		Action actionStealth = new Action(this.getClass(), depth, ActionType.STEALTH, buildObjective(objectives, new ObjectiveHelper(OBJECTIVE, PNJ)), false);
+		Action actionTake = new Action(this.getClass(), depth, ActionType.TAKE, buildObjective(objectives, new ObjectiveHelper(PNJ, PNJ), new ObjectiveHelper(OBJ_GET, OBJ_GET)), false);
 
 		return new Quest(actionGoto, actionStealth, actionTake);
 	}
